05a3edd6b255514186ae1462cf8e528e16166ea2
SOBright <bagasjulion@gmail.com>
2024-03-03 18:05:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+window.setTimeout("waktu()", 1000);
+
+function waktu() {
+    var tanggallengkap = new String(),
+    waktu          	   = new Date();
+    
+    var namahari       = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu"),
+    namahari           = namahari.split(" "),
+    namabulan          = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember"),
+    namabulan          = namabulan.split(" ");
+    
+    var hari           = waktu.getDay();
+    var tanggal        = waktu.getDate();
+    var bulan          = waktu.getMonth();
+    var tahun          = waktu.getFullYear();
+
+    tanggallengkap     = namahari[hari] + ", " + (tanggal < 10 ? "0" +tanggal : tanggal) + " " + namabulan[bulan] + " " + tahun;
+
+    setTimeout("waktu()", 1000);
+
+    var sekarang = "<strong>" + tanggallengkap + "</strong> ,  " + (waktu.getHours() < 10 ? "0" + waktu.getHours() : waktu.getHours()) + ":" + (waktu.getMinutes() < 10 ? "0"+waktu.getMinutes() : waktu.getMinutes()) + ":" + (waktu.getSeconds() < 10 ? "0"+waktu.getSeconds() : waktu.getSeconds());
+
+    document.querySelector(".main-footer > .datetime > span").innerHTML = sekarang;
+} 
+
+let togglebutton = document.getElementById("toggle-sidebar-menu"),
+sidebar = document.getElementsByClassName("sidebar");
+
+togglebutton.addEventListener("click", function ()  {
+    let classList = sidebar[0].classList;
+    classList.toggle("hide");
+});
